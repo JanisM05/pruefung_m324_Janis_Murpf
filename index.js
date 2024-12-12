edd50f79847df57779c6fe3999818f7bd8fbb2b0
JanisM05 <116655274+JanisM05@users.noreply.github.com>
2024-12-12 14:31:44 +0100

    formatter setup

--- index.js
+++ index.js
@@ -2,8 +2,3 @@ import chalk from 'chalk';
 
 console.log(chalk.blue('It works'))
 
-let myUnusedVariable = 42; // Fehler: 'myUnusedVariable' ist deklariert, wird aber nicht verwendet (no-unused-vars)
-
-if (myUnusedVariable == '42') { // Fehler: Es wird '==' anstelle von '===' verwendet (eqeqeq)
-  console.log('This will not run');
-}
