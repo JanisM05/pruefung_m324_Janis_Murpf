bc554fb903ce288213a3d0244cee32b4dafe868f
JanisM05 <116655274+JanisM05@users.noreply.github.com>
2024-12-12 14:30:03 +0100

    linter test

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import chalk from 'chalk';
 
 console.log(chalk.blue('It works'))
+
+let myUnusedVariable = 42; // Fehler: 'myUnusedVariable' ist deklariert, wird aber nicht verwendet (no-unused-vars)
+
+if (myUnusedVariable == '42') { // Fehler: Es wird '==' anstelle von '===' verwendet (eqeqeq)
+  console.log('This will not run');
+}
